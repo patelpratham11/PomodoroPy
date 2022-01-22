d0bcdb73aa1a50d3b41f6ffc648fa788cf57268a
patelpratham11 <ppatel011102@gmail.com>
2022-01-22 15:26:39 -0500

    worked on init screen

--- Pomodoro.py
+++ Pomodoro.py
@@ -1,16 +1,32 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import *
-import sys
+# Import Statements
+from tkinter import *
 
 
-app = QApplication(sys.argv)
-win = QMainWindow()
-win.setGeometry(200, 200, 400, 400)
-win.setWindowTitle("Test")
-box = QCheckBox()
-box.setCheckState(Qt.Checked)
-box.stateChanged.connect(app.show_state)
-app.setCentralWidget(box)
+# mainWindow = Tk()
+# mainWindow.geometry("300x300")
+# mainWindow.title("Pomodoro Timer")
+#
+# def test():
+#     print('hi')
+#
+#
+#
+# Label(mainWindow, text="Pomodoro Timer", fg="white", bg="black", font="Menlo 28", cursor="watch").place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.1, anchor=SW)
+#
+# Button(mainWindow, text='Okay', command=test).place(relx=0.1, rely=0.3, relwidth=0.2, height=20)
+#
 
-win.show()
-sys.exit(app.exec_())
+
+def windowSetUp(name, geometry, title, bg):
+    name = Tk()
+    name.geometry(geometry)
+    name.title(title)
+    name.configure(background=bg)
+    name.resizable(False, False)
+    return name
+
+
+initial = windowSetUp("initial", "400x500", "Pomodoro", "#F15F6A")
+Label(initial, text="Pomodoro Timer", fg="#657EEB", font="Menlo 25", bg="#F15F6A", borderwidth=2, padx=10, pady=20, justify=CENTER).place(relx=0.2, rely=0.2, relwidth=0.6, height=30)
+Label()
+initial.mainloop()
