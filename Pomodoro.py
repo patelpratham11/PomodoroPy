7a4a4d6a4f62c543a70c0583df06d3e26e0b88e8
patelpratham11 <ppatel011102@gmail.com>
2022-01-22 21:59:35 -0500

    worked on initial screen

--- Pomodoro.py
+++ Pomodoro.py
@@ -24,9 +24,25 @@ def windowSetUp(name, geometry, title, bg):
     name.configure(background=bg)
     name.resizable(False, False)
     return name
+def startTimer():
+    work = work.get()
+    rest = rest.get()
+    iterate = longbreak.get()
+def test():
+    pass
 
 
+## initial Window SetUp
 initial = windowSetUp("initial", "400x500", "Pomodoro", "#F15F6A")
-Label(initial, text="Pomodoro Timer", fg="#657EEB", font="Menlo 25", bg="#F15F6A", borderwidth=2, padx=10, pady=20, justify=CENTER).place(relx=0.2, rely=0.2, relwidth=0.6, height=30)
-Label()
+
+work =  IntVar()
+rest = IntVar()
+longbreak = IntVar()
+Label(initial, text="Pomodoro Timer", fg="#0984e3", font="Menlo 25", bg="#F15F6A", borderwidth=2, padx=10, pady=20, justify=CENTER).place(relx=0.2, rely=0.1, relwidth=0.6, height=30)
+
+Scale(initial, label="Work Time (min)",from_=20, to=70, fg="#0984e3", font="Menlo", bg="#F15F6A", cursor="circle", troughcolor="#ff7675", bd=0, variable=work, orient=HORIZONTAL, resolution=5).place(relx=0.2, rely=0.25, relwidth=0.6)
+Scale(initial, label="Break Time (min)",from_=0, to=10, fg="#0984e3", font="Menlo", bg="#F15F6A", cursor="circle", troughcolor="#ff7675", bd=0, variable=rest, orient=HORIZONTAL).place(relx=0.2, rely=0.4, relwidth=0.6)
+Scale(initial, label="Times until Long Break",from_=3, to=10, fg="#0984e3", font="Menlo", bg="#F15F6A", cursor="circle", troughcolor="#ff7675", bd=0, variable=longbreak, orient=HORIZONTAL).place(relx=0.2, rely=0.55, relwidth=0.6)
+
+Button(initial, text="GO", command=startTimer, activebackground="#74b9ff", bg="#0984e3", justify=CENTER).place(relx=0.2, rely=0.7, relwidth=0.2)
 initial.mainloop()
